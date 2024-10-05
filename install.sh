53180071807f03ac8c27f1dee6f5cfd78732e561
Phillip Nguyen <png9981@gmail.com>
2024-10-05 17:52:21 +0700

    feat: update install bash

--- install.sh
+++ install.sh
@@ -2,8 +2,22 @@
 
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-cp ./tmux.conf $HOME/.tmux.conf
-cp ./zshrc $HOME/.zshrc
-source $HOME/.zshrc
+cp ./tmux.conf ~/.tmux.conf
+cp ./zshrc ~/.zshrc
+cp ./gitconfig ~/.gitconfig
+cp ./wezterm.lua ~/.wezterm.lua
+
+
+# Install Dracula theme for Wezterm
+WEZTERM_CONFIG_DIR=~/.config/wezterm/qwer
+# Maksure Wezterm config is existed
+if [ ! -d $WEZTERM_CONFIG_DIR ]; then
+  mkdir -p $WEZTERM_CONFIG_DIR
+fi
+
+cp ./wezterm.lua $WEZTERM_CONFIG_DIR/wezterm.lua
+cp ./dracula-theme/wezterm/colors.toml $WEZTERM_CONFIG_DIR/colors.toml
+
+source ~/.zshrc
 
 
